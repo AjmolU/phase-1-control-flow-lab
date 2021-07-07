1d85898275f4e2ad04a5f9c4d22fda0beb718f2c
Ajmol Uddin <uddin.ajmol@gmail.com>
2021-07-07 15:32:16 -0400

    Completed assignment.

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+const onMe = 'This one is on me!';
+const thirtyBucks = 'I will gladly take your thirty bucks.';
+const noCanDo = 'No can do.';
+const nyc = 'NYC';
+const soundsGood = 'Ok, sounds good.';
+const noGo = 'No go.';
+const thankYouSoMuch = 'Thank you so much.';
+const thankYou = 'Thank you.';
+const bye = 'Bye.';
+const notGenerous = 'not as generous';
+const generous = 'generous';
+
+function scuberGreetingForFeet(ft){
+  if (ft <= 400) {
+    return onMe;
+  } else if (ft >= 2000 && ft <= 2500) {
+    return thirtyBucks;
+  } else {
+    return noCanDo;
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const c = city.toUpperCase();
+  if (c === nyc) {
+    return soundsGood;
+  } else {
+    return noGo;
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  const Ctip = tip.toLowerCase;
+  if (tip === generous) {
+    return thankYouSoMuch;
+  } else if (tip === notGenerous) {
+    return thankYou;
+  } else {
+    return bye;
+  }
 }
